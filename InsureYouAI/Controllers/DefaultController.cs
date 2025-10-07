68cfd306499590ef9562c00c9151e4bae79347e0
EsraTosun <esratosun171@gmail.com>
2025-10-07 23:55:08 +0300

    feat: Implemented homepage design and layout for insurance project

--- InsureYouAI/Controllers/DefaultController.cs
+++ InsureYouAI/Controllers/DefaultController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsureYouAI.Context;
+using InsureYouAI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace InsureYouAI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly InsureContext _context;
+        public DefaultController(InsureContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SubscribeEmail()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult SubscribeEmail(string email)
         {
             return View();
         }
     }
 }
+//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
